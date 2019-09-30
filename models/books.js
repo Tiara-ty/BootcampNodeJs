2c82eaece0eb809b579055b1ae8fbfc806c1d358
tiara rahayu pratiwi <tiara rahayu pratiwi>
2019-09-30 15:59:53 +0700

    belajar mongo db 1

---
+++ models/books.js
@@ -0,0 +1,28 @@
+/** 
+ * Book schema
+ */
+
+ const mongoose = require ("mongoose")
+ const Schema = mongoose.Schema
+
+ let bookSchema = new Schema({
+     title : String,
+     description : String,
+     price : {
+         type : Number,
+         default : null
+     },
+
+     created_at : {
+         type : Date,
+         dafault : Date.now
+     },
+     updated_at : {
+         type : Date,
+         default : Date.now
+     }
+ })
+
+ let Book = mongoose.model("Book", bookSchema)
+ 
+ module.exports = Book
---
+++ routes/books.js
@@ -0,0 +1,23 @@
+const express = require (`express`)
+const router = express.Router()
+const BookModel = require("../models/books")
+
+router.post ("/", (req,res) => {
+    let {title, description} = req.body 
+    var insert_data = {
+        title,
+        description
+    }
+
+    let data = new BookModel(insert_data)
+    data.save()
+
+    return res.send ({
+        status : "success",
+        data,
+        message : "Book created succesfully!"
+    })
+
+})
+
+module.exports = router
