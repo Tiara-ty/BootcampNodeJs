23b9da9671f00b7efa1e0a81ea397bf88d97f751
tiara rahayu pratiwi <tiara rahayu pratiwi>
2019-10-01 12:17:13 +0700

    insert and get data

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 const express = require (`express`)
 const router = express.Router()
+// const {create, getAll} = require("../actions/books")
 const BookModel = require("../models/books")
+// const {isString} = require("lodash")
 
 router.post ("/", (req,res) => {
     let {title, description} = req.body 
@@ -20,4 +22,32 @@ router.post ("/", (req,res) => {
 
 })
 
-module.exports = router
+//  const get = async() => {
+
+//     let data = await BookModel.find({}).exec()
+
+//     return data
+//     }
+
+router.get ("/", async (req,res) => {
+    let hasil = await BookModel.find({}).exec()
+
+    return res.send ({
+        status : "success",
+        hasil,
+        message : "List book"
+    })
+
+})
+
+router.get("/detail", async (req,res) => {
+    let query = await BookModel.find({title : `aku`})
+    return res.send ({
+        status : "success",
+        query,
+        message : "Didapatkan"
+    })
+})
+
+module.exports =
+    router
