66c0ad2a97053e318e6e4473f315edf2462f8544
tiara rahayu pratiwi <tiara rahayu pratiwi>
2019-10-09 15:41:45 +0700

    pagination

--- models/shop.model.js
+++ models/shop.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const mongoosePaginate = require("mongoose-paginate")
 
 let shopSchema = new Schema({
     name: String,
@@ -14,7 +15,12 @@ let shopSchema = new Schema({
         default: Date.now()
     }
 })
-
+shopSchema.plugin(mongoosePaginate)
+shopSchema.virtual('user', {
+    localField: 'author',
+    foreignField: '_id',
+    justOnce: false
+})
 let Shop = mongoose.model("Shop", shopSchema)
 
 module.exports = Shop
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -5,37 +5,46 @@ const CreateShop = require("../action/shops/create.action")
 const UpdateShop = require("../action/shops/update.action")
 const DeleteShop = require("../action/shops/delete.action")
 const SearchShop = require("../action/shops/search.action")
+const ListShop = require("../action/shops/list.action")
 
 router.get("/", async (req, res, next) => {
-    let {name,description,owner} = req.query
-
     try {
         let params = {}
+        let search = {}
 
-        if (name) {
-            params.name = name
+        let limit = parseInt(req.query.limit)
+        if(!limit) {
+            params.limit = 5
+        } else {
+            params.limit = limit
         }
 
-        if (description) {
-            params.description = description
+        let page = parseInt(req.query.page)
+        if(!page) {
+            params.page = 1
+        } else {
+            params.page = page
         }
 
-        if (owner) {
-            params.owner = owner
+        let data = await new ListShop(search, params).exec()
+        let meta = {
+            total: data.total,
+            limit: data.limit,
+            page: data.page,
+            pages: data.pages
         }
+        data = data.data
 
-        let data = await new SearchShop(params).getAll()
-        console.log(params)
-        
         return res.status(200).json({
             status: "success",
+            message: "Get all shop data",
             data,
-            message: "Get detail of shop"
+            meta
         })
     } catch(err) {
         return res.status(400).json({
             status: "error",
-            message: "Data not found"
+            message: err.message
         })
     }
 })
